64c6da5d8595cffb88622f52b8609cad297f5678
Tiago <acrossicauda@hotmail.com.br>
2020-07-05 05:04:40 -0300

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,17 @@
 // import { StatusBar } from 'expo-status-bar';
+//import React, { useState } from 'react';
 import React from 'react';
+
+import './global.css';
 import { StyleSheet } from 'react-native';
 
-import Routes from './src/routes';
-
+import Routes from './routes';
 
 function App() {
   return (
     <Routes />
   );
 }
-
-export default App;
 
 
 const styles = StyleSheet.create({
@@ -22,22 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-//import React, { useState } from 'react';
-import React from 'react';
-
-import './global.css';
-
-import Routes from './routes';
-
-function App() {
-    // Array [valor, funcaoDeAtualizacao]
-    //const [counter, setCounter] = useState(0);
-    //setCounter(counter + 1);
-    // <button onClick={increment}>Incrementar</button>
-
-    return (
-        <Routes />
-    );
-}
 
 export default App;
