63d6173eaa19b3b4db3e69c24c9c5d3aff8eb478
Tiago <acrossicauda@hotmail.com.br>
2020-07-05 05:34:02 -0300

    routes

--- frontend/routes.js
+++ frontend/routes.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
 
 import Testes from './src/pages/Testes';
 
 export default function Routes() {
-    return (
-        <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
 
+    const Stack = createStackNavigator();
+
+    return (
        <BrowserRouter>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home">
+                    <Stack.Screen name="Home" component={Home} />
+                    <Stack.Screen name="Details" component={DetailsScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+
             <Switch>
                 <Route path="/teste" exact component={Testes} />
             </Switch>
