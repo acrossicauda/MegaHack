9ee56820748401fd8b79c6a47bc1fd0925bb8c46
Tiago <acrossicauda@hotmail.com.br>
2020-07-05 03:35:42 -0300

    css global e rotas
    Criei um css global pra toda a aplicação e um js pra configurar as rotas

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+//import React, { useState } from 'react';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text> Mega Hack 2020 !!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import './global.css';
+
+import Routes from './routes';
+
+function App() {
+    // Array [valor, funcaoDeAtualizacao]
+    //const [counter, setCounter] = useState(0);
+    //setCounter(counter + 1);
+    // <button onClick={increment}>Incrementar</button>
+
+    return (
+        <Routes />
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
