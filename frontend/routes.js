bdba62c740e55dce1e25b6a02b2d7da6d52f575c
Valdir Silva <37387797+Valdir2018@users.noreply.github.com>
2020-07-05 05:15:17 -0300

    Commit item de menu
    Comitando itens de menu

--- frontend/routes.js
+++ frontend/routes.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+const Stack = createStackNavigator();
+
 import Testes from './src/pages/Testes';
 
 export default function Routes() {
     return (
-        <BrowserRouter>
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+       <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Testes} />
+                <Route path="/teste" exact component={Testes} />
             </Switch>
-        </BrowserRouter>
+       </BrowserRouter>
+
+        
     );
 }
